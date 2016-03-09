fd2ec68bbbb42581f72420f23eae28ab9873b68c
ching-fu <gavaniyu@hotmail.com>
2016-03-09 13:22:55 +0800

    comment

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,10 +9,7 @@ class EventsController < ApplicationController
 	def show
 		@event = Event.find(params[:id])
 		@page_title = @event.name
-	end
 
-	def edit
-		@event = Event.find(params[:id])
 	end
 
 	def create
@@ -22,9 +19,14 @@ class EventsController < ApplicationController
 		redirect_to :action => :index
 	end
 
+	def edit
+		@event = Event.find(params[:id])
+	end
+
 	def update
 		@event = Event.find(params[:id])
 		@event.update(event_params)
+		#ask client to require /events/show/:id
 		redirect_to :action => :show, :id => @event
 	end
 
@@ -36,6 +38,7 @@ class EventsController < ApplicationController
 
 	private
 	#for Strong Parameters
+	#to read :name & :description from params[:event]
 	def event_params
   		params.require(:event).permit(:name, :description)
 	end
--- db/migrate/20160308235403_add_status_to_events.rb
+++ db/migrate/20160308235403_add_status_to_events.rb
@@ -1,5 +1,6 @@
 class AddStatusToEvents < ActiveRecord::Migration
   def change
   	add_column :events, :status, :string
+  	#add column "status", "string" type into db "events" 
   end
 end
