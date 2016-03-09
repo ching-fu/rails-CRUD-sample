a487b09e3335227b3b5e99830f70a52596cfb6c9
ching-fu <gavaniyu@hotmail.com>
2016-03-09 08:08:40 +0800

    add route, modify DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308233955) do
+ActiveRecord::Schema.define(version: 20160308235403) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160308233955) do
     t.integer  "capacity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
 end
