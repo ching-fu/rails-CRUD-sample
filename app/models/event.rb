e6592502c93694ce59e2083c44056bf8831a076a
ching-fu <gavaniyu@hotmail.com>
2016-03-14 16:50:15 +0800

    pratice ActionRecord: NbyN

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
 	validates_presence_of :name
-	has_many :attendees
-	has_one :location
+	has_many :event_groups
+	has_many :groups, :through => :event_groups
+	has_many :attendees		
+	has_one :location	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308235403) do
+ActiveRecord::Schema.define(version: 20160314073751) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_groups", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +35,19 @@ ActiveRecord::Schema.define(version: 20160308235403) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
