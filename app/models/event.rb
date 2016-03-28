987f7c4a72a33a012e9305379fd5aa3b7ac7b12b
ching-fu <gavaniyu@hotmail.com>
2016-03-28 16:29:49 +0800

    try upload non-image file

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,5 +14,7 @@ class Event < ActiveRecord::Base
 	has_many :users, :through => :memberships
 	has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
+#	has_attached_file :logo	
+#	do_not_validate_attachment_file_type :logo
 
 end
