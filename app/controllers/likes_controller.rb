2e80ac7c7a4a10f3f981628437f20fa3fe6ac1ba
ching-fu <gavaniyu@hotmail.com>
2016-04-01 17:35:13 +0800

    AJAX: Create & Destroy of Like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,22 @@ class LikesController < ApplicationController
 	def create
 		if @event.is_user_liked(current_user)
 		else
-			like=@event.likes.create!(:user=>current_user)
+			@like=@event.likes.create!(:user=>current_user)
 #			like=Like.new(:user=>current_user,:event=>@event)
-			like.save
+			@like.save
 		end
-		redirect_to :back
+		respond_to do |format|
+			format.html { redirect_to :back }
+			format.js { render "like" }#direct to like.js.erb
+		end
 	end
 	def destroy
 		@event.likes.find_by_user_id(current_user).destroy
-		redirect_to :back		
-
+		@like=nil# make events/_like.html.erb to show Unlike button
+		respond_to do |format|
+			format.html { redirect_to :back }
+			format.js { render "like" }#direct to like.js.erb
+		end
 	end
 	private
 	def find_event
